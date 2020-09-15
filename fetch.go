17083862dc6d9286b6bfaaa79d213a9eb5057cea
Bryce Mecum <petridish@gmail.com>
2020-09-15 01:23:10 -0800

    Add routine for fetching server list

---
+++ fetch.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"encoding/xml"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+// ServerList ...
+type ServerList struct {
+	XMLName xml.Name     `xml:"ArrayOfServerItem"`
+	Servers []ServerItem `xml:"ServerItem"`
+}
+
+// ServerItem ...
+type ServerItem struct {
+	XMLname     xml.Name `xml:"ServerItem"`
+	ID          string   `xml:"id"`
+	Description string   `xml:"description"`
+	Emu         string   `xml:"emu"`
+	Host        string   `xml:"server_host"`
+	Port        string   `xml:"server_port"`
+	Type        string   `xml:"type"`
+	Status      string   `xml:"status"`
+	Website     string   `xml:"website_url"`
+	Discord     string   `xml:"discord_url"`
+}
+
+func main() {
+	resp, err := http.Get("https://raw.githubusercontent.com/acresources/serverslist/master/Servers.xml")
+
+	if err != nil {
+		fmt.Println("error")
+	}
+
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	sl := &ServerList{}
+
+	if err := xml.Unmarshal(body, &sl); err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Print("Number of servers is...", len(sl.Servers), "\n")
+
+	for i, server := range sl.Servers {
+		fmt.Println(i, server.ID)
+	}
+}
