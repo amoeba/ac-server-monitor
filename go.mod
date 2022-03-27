5e7ad9d7bc33f960997814e2615661e757a0051b
Bryce Mecum <petridish@gmail.com>
2022-03-26 22:23:19 -0800

    Add in testify for testing

--- go.mod
+++ go.mod
@@ -8,4 +8,5 @@ require (
 	github.com/prometheus/client_golang v1.12.1 // indirect
 	github.com/robfig/cron v1.2.0 // indirect
 	github.com/robfig/cron/v3 v3.0.1
+	github.com/stretchr/testify v1.7.1 // indirect
 )
--- lib/list_test.go
+++ lib/list_test.go
@@ -3,58 +3,34 @@ package lib
 import (
 	"errors"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestGetStatusUp(t *testing.T) {
-	var result string
-
-	// UP
-	result = getStatusMessage(true, nil)
-
-	if result != UP {
-		t.Fail()
-	}
+	result := getStatusMessage(true, nil)
+	assert.Equal(t, result, UP)
 }
+
 func TestGetStatusDown(t *testing.T) {
-
-	// DOWN
 	result := getStatusMessage(false, nil)
-
-	if result != DOWN {
-		t.Fail()
-	}
+	assert.Equal(t, result, DOWN)
 }
+
 func TestGetStatusIOTimeout(t *testing.T) {
-
-	// I/O Timeout
 	timeoutErr := errors.New("i/o timeout")
-
 	result := getStatusMessage(false, timeoutErr)
-
-	if result != DOWN {
-		t.Fail()
-	}
+	assert.Equal(t, result, DOWN)
 }
+
 func TestGetStatusConnRefused(t *testing.T) {
-
-	// Connection Refused
 	connRefusedErr := errors.New("read: connection refused")
-
 	result := getStatusMessage(false, connRefusedErr)
-
-	if result != DOWN {
-		t.Fail()
-	}
+	assert.Equal(t, result, DOWN)
 }
+
 func TestGetStatusError(t *testing.T) {
-
-	// Error
-	// Connection Refused
 	fallbackErr := errors.New("???")
-
 	result := getStatusMessage(false, fallbackErr)
-
-	if result != ERROR {
-		t.Fail()
-	}
+	assert.Equal(t, result, ERROR)
 }
